9ceefa7fa752fa475caeee534c53f222ec4639dd
jha19mayank <60752983+jha19mayank@users.noreply.github.com>
2021-05-10 13:30:22 +0530

    docs: updated docs

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-//  For Player 1----------------------------------------------------------
+// Global variable
 
 var banda1 = {
   turnSco: 0,
   totalSco: 0,
 };
 
+// var banda2 = {
+//   turnSco: 0,
+//   totalSco: 0,
+// };
+
 // wired dice to roll and give different number
 document.getElementById("p1-roll").addEventListener("click", p1Roll);
 document.getElementById("p1-hold").addEventListener("click", p1hold);
+// document.getElementById("p2-roll").addEventListener("click", p2Roll);
+// document.getElementById("p2-hold").addEventListener("click", p2hold);
 
 function p1Roll() {
   var rollNum = Math.floor(Math.random() * 6) + 1;
@@ -31,11 +38,16 @@ function p1hold() {
   banda1.totalSco += banda1.turnSco;
   document.getElementById("p1-total-score").innerHTML = banda1.turnSco;
 
-  banda1.turnSco = 0;
-  document.getElementById("p1-current-score").innerHTML = 0;
+  if (banda1.totalSco >= 100) {
+    alert("Hey!  Player-1 wins!!!🥳");
+    document.location.reload();
+  } else {
+    banda1.turnSco = 0;
+    document.getElementById("p1-current-score").innerHTML = 0;
 
-  // switching turn of players
-  switchturn();
+    // switching turn of players
+    switchturn();
+  }
 }
 
 function switchturn() {
@@ -47,4 +59,49 @@ function switchturn() {
   document.getElementById("p2-roll").disabled = false;
   document.getElementById("p2-hold").disabled = false;
 }
+
 // For Player 2------------------------------------------------------------------
+
+// function p2Roll() {
+//   var rollNum = Math.floor(Math.random() * 6) + 1;
+//   document.getElementById("p2-roll-img").src = "dice/dice-" + rollNum + ".png";
+
+//   // Checking what if we rolled 1 or not
+//   if (rollNum != 1) {
+//     banda2.turnSco += rollNum;
+//     document.getElementById("p2-current-score").innerHTML = banda2.turnSco;
+//   } else {
+//     banda2.turnSco = 0;
+//     document.getElementById("p2-current-score").innerHTML = banda2.turnSco;
+
+//     // switching turn of players
+//     switchturn();
+//   }
+// }
+
+// function p2hold() {
+//   // adding current score to the total score
+//   banda2.totalSco += banda2.turnSco;
+//   document.getElementById("p2-total-score").innerHTML = banda2.turnSco;
+
+//   if (banda2.totalSco >= 100) {
+//     alert("Hey!  Player-2 wins!!!🥳");
+//     document.location.reload();
+//   } else {
+//     banda2.turnSco = 0;
+//     document.getElementById("p2-current-score").innerHTML = 0;
+
+//     // switching turn of players
+//     switchturn();
+//   }
+// }
+
+// function switchturn() {
+//   document.getElementById("p2-header").classList.remove("active");
+//   document.getElementById("p1-header").classList.add("active");
+
+//   document.getElementById("p2-roll").disabled = true;
+//   document.getElementById("p2-hold").disabled = true;
+//   document.getElementById("p1-roll").disabled = false;
+//   document.getElementById("p1-hold").disabled = false;
+// }
