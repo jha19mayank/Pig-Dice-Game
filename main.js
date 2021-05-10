fdf39860acc1b743e669ef6e35fa2a92f521c95b
jha19mayank <60752983+jha19mayank@users.noreply.github.com>
2021-05-10 18:49:05 +0530

    added player 2 functionality

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ var banda1 = {
   totalSco: 0,
 };
 
-// var banda2 = {
-//   turnSco: 0,
-//   totalSco: 0,
-// };
+var banda2 = {
+  turnSco: 0,
+  totalSco: 0,
+};
 
-// wired dice to roll and give different number
+// wired dice to roll and also to hold give different number
 document.getElementById("p1-roll").addEventListener("click", p1Roll);
 document.getElementById("p1-hold").addEventListener("click", p1hold);
-// document.getElementById("p2-roll").addEventListener("click", p2Roll);
-// document.getElementById("p2-hold").addEventListener("click", p2hold);
+document.getElementById("p2-roll").addEventListener("click", p2Roll);
+document.getElementById("p2-hold").addEventListener("click", p2hold);
 
 function p1Roll() {
   var rollNum = Math.floor(Math.random() * 6) + 1;
@@ -29,7 +29,7 @@ function p1Roll() {
     document.getElementById("p1-current-score").innerHTML = banda1.turnSco;
 
     // switching turn of players
-    switchturn();
+    switchturn1();
   }
 }
 
@@ -46,11 +46,11 @@ function p1hold() {
     document.getElementById("p1-current-score").innerHTML = 0;
 
     // switching turn of players
-    switchturn();
+    switchturn1();
   }
 }
 
-function switchturn() {
+function switchturn1() {
   document.getElementById("p1-header").classList.remove("active");
   document.getElementById("p2-header").classList.add("active");
 
@@ -62,46 +62,46 @@ function switchturn() {
 
 // For Player 2------------------------------------------------------------------
 
-// function p2Roll() {
-//   var rollNum = Math.floor(Math.random() * 6) + 1;
-//   document.getElementById("p2-roll-img").src = "dice/dice-" + rollNum + ".png";
+function p2Roll() {
+  var rollNum = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("p2-roll-img").src = "dice/dice-" + rollNum + ".png";
 
-//   // Checking what if we rolled 1 or not
-//   if (rollNum != 1) {
-//     banda2.turnSco += rollNum;
-//     document.getElementById("p2-current-score").innerHTML = banda2.turnSco;
-//   } else {
-//     banda2.turnSco = 0;
-//     document.getElementById("p2-current-score").innerHTML = banda2.turnSco;
+  // Checking what if we rolled 1 or not
+  if (rollNum != 1) {
+    banda2.turnSco += rollNum;
+    document.getElementById("p2-current-score").innerHTML = banda2.turnSco;
+  } else {
+    banda2.turnSco = 0;
+    document.getElementById("p2-current-score").innerHTML = banda2.turnSco;
 
-//     // switching turn of players
-//     switchturn();
-//   }
-// }
+    // switching turn of players
+    switchturn2();
+  }
+}
 
-// function p2hold() {
-//   // adding current score to the total score
-//   banda2.totalSco += banda2.turnSco;
-//   document.getElementById("p2-total-score").innerHTML = banda2.turnSco;
+function p2hold() {
+  // adding current score to the total score
+  banda2.totalSco += banda2.turnSco;
+  document.getElementById("p2-total-score").innerHTML = banda2.turnSco;
 
-//   if (banda2.totalSco >= 100) {
-//     alert("Hey!  Player-2 wins!!!🥳");
-//     document.location.reload();
-//   } else {
-//     banda2.turnSco = 0;
-//     document.getElementById("p2-current-score").innerHTML = 0;
+  if (banda2.totalSco >= 100) {
+    alert("Hey!  Player-2 wins!!!🥳");
+    document.location.reload();
+  } else {
+    banda2.turnSco = 0;
+    document.getElementById("p2-current-score").innerHTML = 0;
 
-//     // switching turn of players
-//     switchturn();
-//   }
-// }
+    // switching turn of players
+    switchturn2();
+  }
+}
 
-// function switchturn() {
-//   document.getElementById("p2-header").classList.remove("active");
-//   document.getElementById("p1-header").classList.add("active");
+function switchturn2() {
+  document.getElementById("p2-header").classList.remove("active");
+  document.getElementById("p1-header").classList.add("active");
 
-//   document.getElementById("p2-roll").disabled = true;
-//   document.getElementById("p2-hold").disabled = true;
-//   document.getElementById("p1-roll").disabled = false;
-//   document.getElementById("p1-hold").disabled = false;
-// }
+  document.getElementById("p2-roll").disabled = true;
+  document.getElementById("p2-hold").disabled = true;
+  document.getElementById("p1-roll").disabled = false;
+  document.getElementById("p1-hold").disabled = false;
+}
