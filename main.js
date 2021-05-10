8be3ac0f84f05b6c8e742a3421096fc40c8f368f
jha19mayank <60752983+jha19mayank@users.noreply.github.com>
2021-05-10 10:51:27 +0530

    📱 made code more accessible

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ var banda1 = {
 };
 
 // wired dice to roll and give different number
-
 document.getElementById("p1-roll").addEventListener("click", p1Roll);
 document.getElementById("p1-hold").addEventListener("click", p1hold);
 
@@ -15,7 +14,6 @@ function p1Roll() {
   document.getElementById("p1-roll-img").src = "dice/dice-" + rollNum + ".png";
 
   // Checking what if we rolled 1 or not
-
   if (rollNum != 1) {
     banda1.turnSco += rollNum;
     document.getElementById("p1-current-score").innerHTML = banda1.turnSco;
@@ -23,21 +21,30 @@ function p1Roll() {
     banda1.turnSco = 0;
     document.getElementById("p1-current-score").innerHTML = banda1.turnSco;
 
-    document.getElementById("p1-header").classList.remove("active");
-    document.getElementById("p2-header").classList.add("active");
-
-    document.getElementById("p1-roll").disabled = true;
-    document.getElementById("p1-hold").disabled = true;
-    document.getElementById("p2-roll").disabled = false;
-    document.getElementById("p2-hold").disabled = false;
+    // switching turn of players
+    switchturn();
   }
 }
 
 function p1hold() {
+  // adding current score to the total score
   banda1.totalSco += banda1.turnSco;
   document.getElementById("p1-total-score").innerHTML = banda1.turnSco;
 
   banda1.turnSco = 0;
-  document.getElementById("p1-current-score").innerHTML = banda1.turnSco;
+  document.getElementById("p1-current-score").innerHTML = 0;
+
+  // switching turn of players
+  switchturn();
+}
+
+function switchturn() {
+  document.getElementById("p1-header").classList.remove("active");
+  document.getElementById("p2-header").classList.add("active");
+
+  document.getElementById("p1-roll").disabled = true;
+  document.getElementById("p1-hold").disabled = true;
+  document.getElementById("p2-roll").disabled = false;
+  document.getElementById("p2-hold").disabled = false;
 }
 // For Player 2------------------------------------------------------------------
